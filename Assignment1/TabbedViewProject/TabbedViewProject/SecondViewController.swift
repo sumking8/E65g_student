545e4a5fe33437f718116d069b95c8ca62cedc02
Andre Chan <mic401@g.harvard.edu>
2017-02-01 16:06:47 +0800

    Assignment 1

---
+++ Assignment1/TabbedViewProject/TabbedViewProject/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  TabbedViewProject
+//
+//  Created by iMac on 31/1/2017.
+//  Copyright © 2017 iMac. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    //@IBOutlet weak var frameSecondSlider: UISlider!
+    //@IBOutlet weak var gridSizeSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
