dc893342628023e0ed756c5148c3932e233d57f3
Andre <mic401@g.harvard.edu>
2017-02-03 12:51:26 +0800

    Assignment 1 - followed Professor's latest video

---
+++ Assignment1/SolutionToAssignment1/SolutionToAssignment1/Simulation.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SolutionToAssignment1
+//
+//  Created by iMac on 3/2/2017.
+//  Copyright © 2017 iMac. All rights reserved.
+//
+
+import UIKit
+
+class Simulation : UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
