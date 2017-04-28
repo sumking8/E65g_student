5776089a8b544946cf3cbb0d37a2af9597b3f435
Andre <mic401@g.harvard.edu>
2017-04-28 19:30:27 +0800

    Add back the missing files.

---
+++ Assignment4/Assignment4/GridView.swift
@@ -0,0 +1,154 @@
+//
+//  GridView.swift
+//  Assignment3
+//
+//  Created by andre on 22/3/2017.
+//  Copyright © 2017 Harvard Division of Continuing Education. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable class GridView : UIView {
+
+    @IBInspectable var size : Int = 10 {
+        didSet {
+            //grid = Grid(size, size)
+        }
+    }
+
+    @IBInspectable var livingColor : UIColor = UIColor.red
+    @IBInspectable var bornColor : UIColor = UIColor.blue
+    @IBInspectable var emptyColor : UIColor = UIColor.green
+    @IBInspectable var diedColor : UIColor = UIColor.yellow
+    @IBInspectable var gridColor : UIColor = UIColor.black
+    @IBInspectable var gridWidth : CGFloat = 2
+
+    //var grid : Grid = Grid(20, 20, cellInitializer: Grid.gliderInitializer)
+    var grid : GridProtocol? {
+        didSet {
+            self.size = (grid?.size.rows)!
+        }
+    }
+    var delegate : ViewDelegate?
+
+    override func draw(_ rect: CGRect) {
+        //draw all the lines first
+        (0 ..< size + 1).forEach {
+            drawLine(
+                start: CGPoint(x: CGFloat($0)/CGFloat(size) * rect.size.width, y: 0.0),
+                end:   CGPoint(x: CGFloat($0)/CGFloat(size) * rect.size.width, y: rect.size.height),
+                lineWidth: gridWidth, lineColor: gridColor
+            )
+            drawLine(
+                start: CGPoint(x: 0.0, y: CGFloat($0)/CGFloat(size) * rect.size.height ),
+                end: CGPoint(x: rect.size.width, y: CGFloat($0)/CGFloat(size) * rect.size.height),
+                lineWidth: gridWidth, lineColor: gridColor
+            )
+        }
+
+        // draw rectangle
+        let cgSize = CGSize(
+            width: rect.size.width / CGFloat(size),
+            height: rect.size.height / CGFloat(size)
+        )
+        let base = rect.origin
+
+        (0 ..< size).forEach { i in
+            (0 ..< size).forEach { j in
+                let origin = CGPoint(
+                    x: base.x + (CGFloat(i) * cgSize.width),
+                    y: base.y + (CGFloat(j) * cgSize.height)
+                )
+                let subRect = CGRect(
+                    origin: origin,
+                    size: cgSize
+                )
+                if let g = grid {
+                    switch g[i, j] {
+                    case .alive :
+                        drawCircle(position: subRect, color: livingColor)
+                        break
+                    case .born :
+                        drawCircle(position: subRect, color: bornColor)
+                        break
+                    case .died :
+                        drawCircle(position: subRect, color: diedColor)
+                        break
+                    case .empty :
+                        drawCircle(position: subRect, color: emptyColor)
+                        break
+                    }
+                }
+            }
+        }
+    }
+
+    func drawCircle(position: CGRect, color:UIColor) {
+        let path = UIBezierPath(ovalIn: position)
+        color.setFill()
+        path.fill()
+    
+    }
+    
+    func drawLine(start:CGPoint, end: CGPoint, lineWidth:CGFloat, lineColor:UIColor) {
+        let path = UIBezierPath()
+        
+        //set the path's line width to the height of the stroke
+        path.lineWidth = lineWidth
+        
+        //move the initial point of the path
+        //to the start of the horizontal stroke
+        path.move(to: start)
+        
+        //add a point to the path at the end of the stroke
+        path.addLine(to: end)
+        
+        lineColor.setFill()
+        
+        //draw the stroke
+        lineColor.setStroke()
+        path.stroke()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //lastTouchedPosition = process(touches: touches)
+        processTouch(touches, with: event)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //lastTouchedPosition = process(touches: touches)
+        processTouch(touches, with: event)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //lastTouchedPosition = nil
+    }
+    
+    private func processTouch(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation : CGPoint = touch.location(in:self)
+
+            let ansX = Float(touchLocation.x) * Float(size) / Float(self.frame.size.width)
+            let ansY = Float(touchLocation.y) * Float(size) / Float(self.frame.size.height)
+            let rX : Int = Int(Float(touchLocation.x).truncatingRemainder(dividingBy:Float(size)))
+            let rY : Int = Int(Float(touchLocation.y).truncatingRemainder(dividingBy: Float(size)))
+            let x : Int = (ansX < 1) ? 0 : ((rX > 0) ? Int(floor(ansX)) : Int(ansX))
+            let y : Int = (ansY < 1) ? 0 : ((rY > 0) ? Int(floor(ansY)) : Int(ansY))
+            if self.grid  != nil {
+                var g = self.grid!
+                g[x, y] = g[x, y].toggle(value: (g[x, y]))
+                delegate?.viewDidUpdate(withGrid: g)
+                let nc = NotificationCenter.default
+                let name = Notification.Name(rawValue: "ViweUpdate")
+                let n = Notification(name: name, object: g, userInfo: ["GridView" : self])
+                nc.post(n)
+                self.grid = g
+            }
+        }
+        setNeedsDisplay()
+    }
+
+}
+
+
---
+++ Assignment4/Assignment4/StatisticsViewController.swift
@@ -0,0 +1,124 @@
+//
+//  StatisticsViewController.swift
+//  Assignment4
+//
+//  Created by iMac on 21/4/2017.
+//  Copyright © 2017 Harvard Division of Continuing Education. All rights reserved.
+//
+
+import UIKit
+
+class StatisticsViewController: UIViewController {
+    
+    @IBOutlet weak var cAlive: UILabel!
+    @IBOutlet weak var cBorn: UILabel!
+    @IBOutlet weak var cDied: UILabel!
+    @IBOutlet weak var cEmpty: UILabel!
+    
+    @IBOutlet weak var aView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        /*
+        let nc = NotificationCenter.default
+        let name = Notification.Name(rawValue: "EngineUpdate")
+        nc.addObserver(forName: name, object: nil, queue: nil) { (n) in
+            self.reset()
+            let engine = n.userInfo?["StandardEngine"] as! EngineProtocol
+            let size = engine.grid.size
+            (0 ..< size.rows).forEach { i in
+                (0 ..< size.cols).forEach { j in
+                    switch engine.grid[i, j] {
+                    case .alive :
+                        self.add(self.cAlive)
+                        break
+                    case .born :
+                        self.add(self.cBorn)
+                        break
+                    case .died :
+                        self.add(self.cDied)
+                        break
+                    case .empty :
+                        self.add(self.cEmpty)
+                        break
+                    }
+
+                }
+            }
+
+        }
+        */
+        registerEngineNotification()
+        registerViewNotification()
+        aView.setNeedsDisplay()
+    }
+    
+    private func registerEngineNotification() {
+        let nc = NotificationCenter.default
+        let name = Notification.Name(rawValue: "EngineUpdate")
+        nc.addObserver(forName: name, object: nil, queue: nil) { (n) in
+            self.reset()
+            let engine = n.userInfo?["StandardEngine"] as! EngineProtocol
+            let size = engine.grid.size
+            self.count(row: size.rows, col: size.cols, g: engine.grid)
+            self.aView.setNeedsDisplay()
+        }
+    }
+    
+
+    private func registerViewNotification() {
+        let nc = NotificationCenter.default
+        let name = Notification.Name(rawValue: "ViweUpdate")
+        nc.addObserver(forName: name, object: nil, queue: nil) { (n) in
+            self.reset()
+            let gridView = n.userInfo?["GridView"] as! GridView
+            let size = gridView.grid?.size
+            self.count(row: (size?.rows)!, col: (size?.cols)!, g: gridView.grid!)
+            self.aView.setNeedsDisplay()
+        }
+        
+    }
+
+    
+    private func count(row: Int, col: Int, g : GridProtocol) {
+        (0 ..< row).forEach { i in
+            (0 ..< col).forEach { j in
+                switch g[i, j] {
+                case .alive :
+                    self.add(self.cAlive)
+                    break
+                case .born :
+                    self.add(self.cBorn)
+                    break
+                case .died :
+                    self.add(self.cDied)
+                    break
+                case .empty :
+                    self.add(self.cEmpty)
+                    break
+                }
+            }
+        }
+        print ("Alive: \(cAlive.text ?? "0") Born: \(cBorn.text ?? "0") Died: \(cDied.text ?? "0") Empty: \(cEmpty.text ?? "0")")
+    }
+    
+    private func reset() {
+        cAlive.text = "0"
+        cBorn.text = "0"
+        cDied.text = "0"
+        cEmpty.text = "0"
+    }
+
+    private func add(_ label : UILabel) {
+        label.text = String(Int(label.text!)! + 1)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
